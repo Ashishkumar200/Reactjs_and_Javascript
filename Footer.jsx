7c3366b815afa82073f8ae2ddf7e7091d3fe6672
Ashishkumar200 <84870651+Ashishkumar200@users.noreply.github.com>
2021-06-06 18:27:46 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  var curryear = new Date().getFullYear();
+
+ return (
+  <footer>
+   <p>Copyright @ (currYear}</p>
+  </footer>
+
+);
+
+}
+
+export default Footer;
